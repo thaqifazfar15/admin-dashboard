21b54cec5fbbad55f498eced537851a9627a1fd1
Thaqif Azfar <thaqifazfar15@gmail.com>
2022-09-16 22:21:31 +0800

    finish styling

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
 // interactive color-label
 
-const colorLabels = document.querySelectorAll(".color-label")
+const colorLabels = document.querySelectorAll(".color-label");
 
-colorLabels.forEach(label => {
-    label.addEventListener("click", function () {
-        isLabelHide(label) ? showLabel(label) : hideLabel(label);
-    })
+colorLabels.forEach((label) => {
+  label.addEventListener("click", function () {
+    isLabelHide(label) ? showLabel(label) : hideLabel(label);
+  });
 });
 
 let isLabelHide = (e) => {
-
-    if (e.style.width == "3.3%") {
-        return true
-    }
-    return false
+  if (e.style.width == "3.3%") {
+    return true;
+  }
+  return false;
 };
 
 let showLabel = (e) => {
-    e.style.transitionDuration = "0.5s"
-    e.style.width = "100%";
-    e.style.borderRadius = "12px"
-    
+  e.style.transitionDuration = "0.5s";
+  e.style.width = "100%";
+  e.style.borderRadius = "12px";
 };
 
 let hideLabel = (e) => {
-    e.style.transitionDuration = "0.5s"
-    e.style.width = "3.3%";
-    e.style.borderRadius = "12px 0 0 12px"
+  e.style.transitionDuration = "0.5s";
+  e.style.width = "3.3%";
+  e.style.borderRadius = "12px 0 0 12px";
 };
